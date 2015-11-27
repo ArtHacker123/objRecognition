53f5e54deabc1d870c62bbca8006e325ee29992f
Leena Chennuru Vankadara <4chennur@informatik.uni-hamburg.de>
2015-11-27 11:08:16 +0100

    Exception handling in detetctordescriptor and preobj. rosscript is the script used for integration with ros.

--- detectorDescriptor.py
+++ detectorDescriptor.py
@@ -2,71 +2,113 @@ import numpy as np
 import cv2
 from matplotlib import pyplot as plt
 
+def unit8Image(imageGrayInput):
+    try:
+        imageGrayInput_unit8 = np.uint8(imageGrayInput)
+        return imageGrayInput_unit8
+    except: 
+        print 'Make sure numpy is imported and is renamed as np. Also make sure your input is a greyscale image'
 
 def featureDescriptorORB(roiImageFiltered, kp):
     orb = cv2.ORB()
-    kp, des = orb.compute(roiImageFiltered, kp)
-    if (np.size(kp)>0):
-        roiKeyPointImage = cv2.drawKeypoints(roiImageFiltered, kp, color=(255, 0, 0))
-        return kp, des, roiKeyPointImage
-    else:
-        return kp, des, roiImageFiltered
+    roiImageFiltered_unit8 = unit8Image(roiImageFiltered)
+    try:
+        kp, des = orb.compute(roiImageFiltered_unit8, kp)
+        roiKeyPointImage_unit8 = cv2.drawKeypoints(roiImageFiltered_unit8, kp, color=(0, 255, 0), flags=0)
+        return kp, des, roiKeyPointImage_unit8
+    except TypeError:
+         print 'The input image in the function featureDescriptorORB is not of data type unit8 or its not grayscale. Plese convert the image to unit8 using numpy.unit8.'
+    except IOError:
+        print 'The path to the file in featureDescriptorORB is not correctly specified. Please check that the file is in the correct location.'
+    else: 
+        print 'The function featureDescriptorORB is not working. You have most likely given invalid arguments.'
+    
 
 
 def featureDetectCorner(roiImageFiltered):
+    roiImageFiltered_unit8 = unit8Image(roiImageFiltered)
     fast = cv2.FastFeatureDetector()
-    kp = fast.detect(roiImageFiltered, None)
-    if (np.size(kp)>0):
-        roiKeyPointImage = cv2.drawKeypoints(roiImageFiltered, kp, color=(255, 0, 0))
-        return kp, roiKeyPointImage
-    else:
-        return kp, roiImageFiltered
+    try:
+        kp = fast.detect(roiImageFiltered_unit8, None)
+        roiKeyPointImage_unit8 = cv2.drawKeypoints(roiImageFiltered_unit8, kp, color=(255, 0, 0))
+        return kp, roiKeyPointImage_unit8
+    except TypeError:
+         print 'The input image in the function featureDetectCorner is not of data type unit8 or its not grayscale. Plese convert the image to unit8 using numpy.unit8.'
+    except IOError:
+        print 'The path to the file in featureDetectCorner is not correctly specified. Please check that the file is in the correct location.'
+    else: 
+        print 'The function featureDetectCorner is not working. You have most likely given invalid arguments.'
 
 
 def featureDetectDesORB(roiImageFiltered):
     orb = cv2.ORB()
-    kp, des = orb.detectAndCompute(roiImageFiltered, None)
-    if (np.size(kp)>0):
-        roiKeyPointImage = cv2.drawKeypoints(roiImageFiltered, kp, color=(255, 0, 0))
-        return kp, des, roiKeyPointImage
-    else:
-        return kp, des, roiImageFiltered
+    roiImageFiltered_unit8 = unit8Image(roiImageFiltered)
+    try:
+        kp, des = orb.detectAndCompute(roiImageFiltered_unit8, None)
+        roiKeyPointImage_unit8 = cv2.drawKeypoints(roiImageFiltered_unit8, kp, None, (255, 0, 0), 2)
+        return kp, des, roiKeyPointImage_unit8
+    except TypeError:
+         print 'The input image in the function featureDetectDesORB is not of data type unit8 or its not grayscale. Plese convert the image to unit8 using numpy.unit8.'
+    except IOError:
+        print 'The path to the file in featureDetectDesORB is not correctly specified. Please check that the file is in the correct location.'
+    else: 
+        print 'The function featureDetectDesORB is not working. You have most likely given invalid arguments.'
+
 
 
 def featureDetectDesSTAR(roiImageFiltered):
     star = cv2.FeatureDetector_create("STAR")
     brief = cv2.DescriptorExtractor_create("BRIEF")
-    kp = star.detect(roiImageFiltered, None)
-    kp, des = brief.compute(roiImageFiltered, kp)
-    if (np.size(kp)>0):
-        roiKeyPointImage = cv2.drawKeypoints(roiImageFiltered, kp, color=(255, 0, 0))
-        return kp, des, roiKeyPointImage
-    else:
-        return kp, des, roiImageFiltered
+    roiImageFiltered_unit8 = unit8Image(roiImageFiltered)
+    try:
+        kp = star.detect(roiImageFiltered_unit8, None)
+        kp, des = brief.compute(roiImageFiltered_unit8, kp)
+        roiKeyPointImage_unit8 = cv2.drawKeypoints(roiImageFiltered_unit8, kp, None, (255, 0, 0), 2)
+        return kp, des, roiKeyPointImage_unit8
+    except TypeError:
+         print 'The input image in the function featureDetectDesSTAR is not of data type unit8 or its not grayscale. Plese convert the image to unit8 using numpy.unit8.'
+    except IOError:
+        print 'The path to the file in featureDetectDesSTAR is not correctly specified. Please check that the file is in the correct location.'
+    else: 
+        print 'The function featureDetectDesSTAR is not working. You have most likely given invalid arguments.'
+        
 
 
 def featureDetectSTAR(roiImageFiltered):
     star = cv2.FeatureDetector_create("STAR")
-    kp = star.detect(roiImageFiltered, None)
-    if (np.size(kp)>0):
-        roiKeyPointImage = cv2.drawKeypoints(roiImageFiltered, kp, color=(255, 0, 0))
-        return kp, roiKeyPointImage
-    else:
-        return kp, roiImageFiltered
+    roiImageFiltered_unit8 = unit8Image(roiImageFiltered)
+    try:
+        kp = star.detect(roiImageFiltered_unit8, None)
+        roiKeyPointImage_unit8 = cv2.drawKeypoints(roiImageFiltered_unit8, kp, None, (255, 0, 0), 2)
+        return kp, roiKeyPointImage_unit8
+    except TypeError:
+         print 'The input image in the function featureDetectSTAR is not of data type unit8 or its not grayscale. Plese convert the image to unit8 using numpy.unit8.'
+    except IOError:
+        print 'The path to the file in featureDetectSTAR is not correctly specified. Please check that the file is in the correct location.'
+    else: 
+        print 'The function featureDetectSTAR is not working. You have most likely given invalid arguments.' 
 
 
 def featureDescriptorBrief(roiImageFiltered, kp):
     brief = cv2.DescriptorExtractor_create("BRIEF")
-    kp, des = brief.compute(roiImageFiltered, kp)
-    if (np.size(kp)>0):
-        roiKeyPointImage = cv2.drawKeypoints(roiImageFiltered, kp, color=(255, 0, 0))
-        return kp, des, roiKeyPointImage
-    else:
-        return kp, des, roiImageFiltered
+    roiImageFiltered_unit8 = unit8Image(roiImageFiltered)
+    try:
+        kp, des = brief.compute(roiImageFiltered_unit8, kp)
+        roiImageFiltered_unit8 = cv2.drawKeypoints(roiImageFiltered_unit8, kp, color=(0, 255, 0), flags=0)
+        return kp, des, roiImageFiltered_unit8
+    except TypeError:
+         print 'The input image in the function featureDescriptorBrief is not of data type unit8 or its not grayscale. Plese convert the image to unit8 using numpy.unit8.'
+    except IOError:
+        print 'The path to the file in featureDescriptorBrief is not correctly specified. Please check that the file is in the correct location.'
+    else: 
+        print 'The function featureDescriptorBrief is not working. You have most likely given invalid arguments.'
 
 
 def kpToHogBox(kp):
-	xValue = kp.pt[0]
-	yValue = kp.pt[1]
+    xValue = kp.pt[0]
+    yValue = kp.pt[1]
 	# length = 16
-	return xValue-8, yValue-8
+    try:
+        return xValue-8, yValue-8
+    except:
+        print 'No key points were found in the image'
--- preObj.py
+++ preObj.py
@@ -1,44 +1,102 @@
 import numpy as np
 import cv2
 from matplotlib import pyplot as plt
+from scipy import misc as msc
 
+
+
+def unit8Image(imageGrayInput):
+    try:
+        imageGrayInput_unit8 = np.uint8(imageGrayInput)
+        return imageGrayInput_unit8
+    except: 
+        print 'Make sure numpy is imported and is renamed as np. Also make sure your input is a greyscale image'
 
 def otsuBin(imageGrayInput):
-    ret, thresh = cv2.threshold(imageGrayInput, 0, 255,
-                                cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
-    return thresh
-
+    imageGrayInput_unit8 = unit8Image(imageGrayInput)
+    try:
+        ret, thresh = cv2.threshold(imageGrayInput_unit8, 0, 255,cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
+        return thresh
+    except TypeError: 
+        print 'The input image in the function otsuBin is not of data type unit8 or its not grayscale. Plese convert the image to unit8 using numpy.unit8.'
+    except IOError:
+        print 'The path to the file in otsuBin is not correctly specified. Please check that the file is in the correct location.'
+    else: 
+        print 'The function otsubin is not working. You have most likely given invalid arguments.'
+    
 
 def meanShift(imageInput):
-    meanShifted = cv2.pyrMeanShiftFiltering(imageInput, 20, 20)
-    return meanShifted
+    imageInput_unit8 = unit8Image(imageInput)
+    try:
+        meanShifted = cv2.pyrMeanShiftFiltering(imageInput_unit8, 20, 20)
+        return meanShifted
+    except TypeError: 
+        print 'The input image in the function meanShift is not of data type unit8. Plese convert the image to unit8 using numpy.unit8.'
+    except IOError:
+        print 'The path to the file in meanShift is not correctly specified. Please check that the file is in the correct location.'
+    else: 
+        print 'The function meanShift is not working. You have most likely given invalid arguments.'
+
 
 
 def cannyEdge(imageInput):
-    edgeDetection = cv2.Canny(imageInput, 50, 50)
-    return edgeDetection
+    ''' Canny edge detctor on an Input image which should be in unit8 format
+    This returns the edgeDetetction variable which contains the image with the edges marked out '''
+    imageInput_unit8 = unit8Image(imageInput)
+    try:
+        edgeDetection = cv2.Canny(imageInput_unit8, 50, 50)
+        return edgeDetection
+    except TypeError: 
+        print 'The input image in the function cannyEdge is not of data type unit8. Plese convert the image to unit8 using numpy.unit8.'
+    except IOError:
+        print 'The path to the file in cannyEdge is not correctly specified. Please check that the file is in the correct location.'
+    else: 
+        print 'The function cannyEdge is not working. You have most likely given invalid arguments.'
 
 
 def adapThresh(imageInput):
-    threshAdaptive = cv2.adaptiveThreshold(imageInput, 255, 1, 1, 11, 2)
-    return threshAdaptive
+    imageInput_unit8 = unit8Image(imageInput)
+    try:
+        threshAdaptive = cv2.adaptiveThreshold(imageInput, 255, 1, 1, 11, 2)
+        return threshAdaptive
+    except TypeError: 
+        print 'The input image in the function adapThresh is not of data type unit8. Plese convert the image to unit8 using numpy.unit8.'
+    except IOError:
+        print 'The path to the file in adapThresh is not correctly specified. Please check that the file is in the correct location.'
+    else: 
+        print 'The function adapThresh is not working. You have most likely given invalid arguments.'
+
 
 
 def contourFind(prepImage):
-    contours, hierarchy = cv2.findContours(prepImage, cv2.RETR_EXTERNAL,
-                                           cv2.CHAIN_APPROX_SIMPLE)																																									
-    return contours, hierarchy
+    prepImage_unit8 = unit8Image(prepImage)
+    try: 
+        contours, hierarchy = cv2.findContours(prepImage_unit8, cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+        return contours, hierarchy
+    except TypeError: 
+        print 'The input image in the function contourFind is not binary. Plese convert the image to binary before using this function.'
+    except IOError:
+        print 'The path to the file in contourFind is not correctly specified. Please check that the file is in the correct location.'
+    else: 
+        print 'The function contourFind is not working. You have most likely given invalid arguments.'																																									
+    
 
 
-def contourFindFull(prepImage):
-    contours, hierarchy = cv2.findContours(prepImage, cv2.RETR_TREE,
-                                           cv2.CHAIN_APPROX_SIMPLE)																																									
-    return contours, hierarchy
+def contourDraw(imageInput, prepImage):
+    prepImage_unit8 = unit8Image(prepImage)
+    imageInput_unit8 = unit8Image(imageInput)
+    try:
+        contours, hierarchy = cv2.findContours(prepImage_unit8, cv2.RETR_LIST,cv2.CHAIN_APPROX_SIMPLE)
+        imageInputCopy = imageInput_unit8.copy()
+        cv2.drawContours(imageInputCopy, contours, -1, (255, 0, 0), 3)
+        return imageInputCopy
+    except TypeError: 
+        print 'The input image in the function contourDraw is not . Plese convert the image to unit8 using numpy.unit8.'
+    except IOError:
+        print 'The path to the file in contourDraw is not correctly specified. Please check that the file is in the correct location.'
+    else: 
+        print 'The function contourDraw is not working. You have most likely given invalid arguments.'
+        
 
+##Testing Phase
 
-def contourDraw(inputImage, prepImage):
-    contours, hierarchy = cv2.findContours(prepImage, cv2.RETR_LIST,
-                                           cv2.CHAIN_APPROX_SIMPLE)
-    inputImageCopy = inputImage.copy()
-    cv2.drawContours(inputImageCopy, contours, -1, (255, 0, 0), 3)
-    return inputImageCopy
